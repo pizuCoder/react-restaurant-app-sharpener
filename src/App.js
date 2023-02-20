6455b83d9ad161b4c960f19cfb8201af36500035
Priya Moghe <102720207+pizuCoder@users.noreply.github.com>
2023-02-20 15:21:24 +0530

    modal open and close state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 // import Header from './components/layout/Header';
-import { Fragment } from 'react';
+import { Fragment, useState }from 'react';
 import Meals from './components/Meals/Meals';
 import Header from './components/Layout/Header';
 import Cart from './components/Cart/Cart';
 
 function App() {
+  const [cartShown, setCartShown] = useState(false)
+
+  const showCartHandler = () => {
+    setCartShown(true)
+  }
+  const hideCartHandler= () =>{
+    setCartShown(false)
+  }
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartShown && < Cart onHideCart={hideCartHandler}/>}
+      <Header onShowCart = {showCartHandler}/>
       <Meals />
     </Fragment>
   );
