751766222fdcc8909ee8635a5919665858d2ed91
Priya Moghe <102720207+pizuCoder@users.noreply.github.com>
2023-02-20 15:05:12 +0530

    Modal added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import { Fragment } from 'react';
 import Meals from './components/Meals/Meals';
 import Header from './components/Layout/Header';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <Meals />
     </Fragment>
